3bbff999338541ef2eef9c04933af3f6043db001
Joytaylor <jtaylor2@imsa.edu>
2020-07-08 19:05:00 -0500

    Updates

---
+++ precache-manifest.08680b2876b8f7987a8c5e50c5b4c0b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288a98a8d05442dcf3429247cff0e236",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d27080cf13c48976f52",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9a044066c621109156a2",
+    "url": "/static/js/2.86d6ef18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.86d6ef18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d27080cf13c48976f52",
+    "url": "/static/js/main.e0992515.chunk.js"
+  },
+  {
+    "revision": "dfd182da2461ddda8af9",
+    "url": "/static/js/runtime-main.f258c7a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
