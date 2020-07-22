6479829e3e9dd3036af9a06df7b3257c32b14e99
Joytaylor <jtaylor2@imsa.edu>
2020-07-22 04:37:18 -0500

    Updates

---
+++ precache-manifest.7a0ec5a59f3ce239c086018690da3632.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1b38ffd3d095c541ec8eded811cdf0",
+    "url": "/testing/index.html"
+  },
+  {
+    "revision": "2c8bfd886628f476f6d4",
+    "url": "/testing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0174b60aaed6be4a5afc",
+    "url": "/testing/static/js/2.ee5e15d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testing/static/js/2.ee5e15d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c8bfd886628f476f6d4",
+    "url": "/testing/static/js/main.3453bb02.chunk.js"
+  },
+  {
+    "revision": "4c6fb260a56b0cc684f5",
+    "url": "/testing/static/js/runtime-main.b33d3aef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testing/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b8835c89218b439198e69ce99e191ed.js"
+  "/testing/precache-manifest.7a0ec5a59f3ce239c086018690da3632.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/testing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
